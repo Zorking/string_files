893c0a7e07b6fb90bd1288ef41ddc562fae8f41c
vfabrichnov <zorumnipa@gmail.com>
2016-11-21 01:54:05 +0300

    Небольшой рефакторинг

--- main.cpp
+++ main.cpp
@@ -3,27 +3,14 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
-
+#include <string>
 using namespace std;
 
 
-void create_file() {
-    int is_correct;
-    string answer;
+void create_file(string data) {
     ofstream newfile;
-    cout << "File is ready: Yes or No?\n";
-    is_correct = 0;
-    while (is_correct == 0) {
-        cin >> answer;
-        if (answer == "No") {
-            newfile.open("qwerty.txt");
-            newfile << answer;
-        } else if (answer != "Yes") {
-            cout << "Yes or No?\n";
-            continue;
-        }
-        is_correct = 1;
-    }
+    newfile.open("qwerty.txt");
+    newfile << data;
     newfile.close();
 }
 
@@ -34,25 +21,38 @@ void read_file() {
     if (file.is_open()) {
         while (getline(file, line)) {
             line = line;
-            all_strings.append(line);
             string word;
             istringstream iss(line, ios_base::in);
             vector<string> wordsVector;
             while (iss >> word) {
                 wordsVector.push_back(word);
                 if (word.find_last_of(word[0]) > 0) {
-                    cout << word + "\n";
+                    all_strings.append(word + " ");
                 }
             }
             reverse(begin(wordsVector), end(wordsVector));
         }
-        cout << all_strings;
         file.close();
     } else cout << "Unable to open file";
 }
 
 int main() {
-    create_file();
+    int is_correct;
+    string answer, data;
+    cout << "File is ready: Yes or No?\n";
+    is_correct = 0;
+    while (is_correct == 0) {
+        cin >> answer;
+        if (answer == "No") {
+            cout << "Enter some data to file\n";
+            cin >> data;
+            create_file(data);
+        } else if (answer != "Yes") {
+            cout << "Yes or No?\n";
+            continue;
+        }
+        is_correct = 1;
+    };
     read_file();
     return 0;
 }
